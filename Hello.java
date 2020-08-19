7711093825e1351e51d9187bdf998e44cca8dffa
NoraKostyk <69921172+NoraKostyk@users.noreply.github.com>
2020-08-19 14:50:31 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/*****************************************************************************
+*Hello.java
+******************************************************************************
+*Nora Kostyk
+*8/19/20
+*CMSC 255 Section 002
+******************************************************************************/
+public class Hello {
+	public static void main(String[] args) {
+	
+		System.out.println("<Nora Kostyk>");
+		System.out.println("<Hello.java>");
+		System.out.println("<Lab 1>");
+		System.out.println("<8/19/20>");
+		System.out.println("<CMCS 255 Section 002>");
+		System.out.println();
+		}
+ }
